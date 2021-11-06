dbc27c1a64b94d691163c7c2bad57ca926a48d40
1w1111795PailosSarquisNicolasFrancisco <69583672+1w1111795PailosSarquisNicolasFrancisco@users.noreply.github.com>
2021-11-06 19:38:51 -0300

    Agrego reporte cliente mas frecuentes

---
+++ Reportes/ClienteMasFrecuente/ReporteClienteMasFrecuente.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    var nombres = [];
+    var cantidades = [];
+
+    $.ajax({
+        url: "https://localhost:5001/Cliente/ObtenerClientes",
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json',
+        success: function (result) {
+            {
+                if (result.ok) {
+                    result.return.forEach(element => {
+                        nombres.push(element.nombre);
+                        cantidades.push(element.cantidadPedidos);
+                    });
+                    const ctx = document.getElementById('myChart').getContext('2d');
+                    const myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: nombres,
+                            datasets: [{
+                                label: 'Cantidad de Compras',
+                                data: cantidades,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true,
+                                }
+                            }
+                        }
+                    });
+                } else {
+                }
+            };
+        },
+        error: function (error) { console.log(error); }
+    });
+});
+
